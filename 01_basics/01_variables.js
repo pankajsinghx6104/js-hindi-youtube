97f723af71b88283d22af27720c02ac99143d339
pankajsinghx6104 <pankajsinghx6104@gmail.com>
2025-02-16 16:24:49 +0530

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -19,6 +19,6 @@ The difference between let and var is in the scope of the variables they create:
 Variables declared by let are only available inside the
 block where they’re defined. Variables declared by var
 are available throughout the function in which they’re
-declared.
+declared. 
 */
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) //To print many values
