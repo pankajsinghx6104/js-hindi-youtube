5b20e2fca09a53696ab6cd6218d3114f809d865e
pankajsinghx6104 <pankajsinghx6104@gmail.com>
2025-02-16 17:17:00 +0530

    conversion of datatypes

--- 01_basics/03_conversion&operation.js
+++ 01_basics/03_conversion&operation.js
@@ -1,5 +1,34 @@
-let score = 33
+//let score = "33abc"
+// let scope = null;  
+let score = "pankaj";
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN (not a number)
+// true =>; false => 0
+
+
+//let isloggedIn = 1
+//let isloggedIn = ""
+let isloggedIn = "pankaj"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "pankaj" => true
+
+
+let someNumber = 33
+
+let string = String(someNumber)
+console.log(string);
+console.log(typeof string);
 
